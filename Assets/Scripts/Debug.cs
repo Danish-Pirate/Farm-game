9dc660fdeea5b6e78c16b48e060252cad7b41669
Danish-Pirate <arjacokaki@hotmail.com>
2023-12-22 15:34:03 +0100

    Refactor and start of player inventory

--- Assets/Scripts/Debug.cs
+++ Assets/Scripts/Debug.cs
@@ -1,16 +1,30 @@
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Debug : MonoBehaviour
-{
+public class Debug : MonoBehaviour {
+    public Text debugInfoUI;
+    public bool isDebugModeEnabled;
+    
+    private float deltaTime;
+    private StringBuilder stringBuilder;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        stringBuilder = new StringBuilder("FPS: ", 5);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.print(1.0f / Time.deltaTime);
+    
+    void Update() {
+        if (isDebugModeEnabled) {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+            float fps = 1.0f / deltaTime;
+            
+            stringBuilder.Clear();
+            stringBuilder.Append("FPS: ").Append(fps.ToString("F2"));
+            debugInfoUI.text = stringBuilder.ToString();
+            
+            debugInfoUI.gameObject.SetActive(true);
+        }
+        else debugInfoUI.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,69 +1,128 @@
-using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+enum MovementDirection {
+    NONE, UP, DOWN, LEFT, RIGHT
+}
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Animator animator;
-    private Vector2 input;
+    private MovementDirection movementDirection;
     private bool isMoving;
+    private Rigidbody2D rb;
+    
+    public Transform movePoint;
+    public Tilemap solidObjects;
 
-    public LayerMask solidObjectsLayer;
-    private BoxCollider2D playerCollider;
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int MoveX = Animator.StringToHash("moveX");
+    private static readonly int MoveY = Animator.StringToHash("moveY");
 
     private void Start() {
-        playerCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        movePoint.SetParent(null);
     }
 
-    void Update()
-    {
-        if (!isMoving)
-        {
-            GetInput();
-            HandleMovement();
-        }
-
-        animator.SetBool("isMoving", isMoving);
+    private void Update() {
+        HandleInput();
     }
 
-    private void HandleMovement()
-    {
-        if (input != Vector2.zero)
-        {
-            animator.SetFloat("moveX", input.x);
-            animator.SetFloat("moveY", input.y);
-            Vector3 targetPos = transform.position + new Vector3(input.x, input.y, 0f);
-            if (IsWalkable(targetPos)) {
-                StartCoroutine(Move(targetPos));
+    private void FixedUpdate() {
+        if (movementDirection != MovementDirection.NONE) {
+            GetTargetPos();
+            if (IsWalkable()) {
+                Move();
             }
         }
+        StartCoroutine(AnimateMovement());
     }
 
-    private bool IsWalkable(Vector3 targetPos) {
-        if (Physics2D.OverlapCircle(targetPos, 0f, solidObjectsLayer)) {
-            return false;
-        }
-        return true;
-    }
-
-    private void GetInput()
-    {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
-
-        if (input.y != 0) input.x = 0;
-    }
-
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.fixedDeltaTime);
-            yield return null;
+    private bool IsWalkable() {
+        Vector3Int solidMapTile = solidObjects.WorldToCell(movePoint.position);
+        if (solidObjects.GetTile(solidMapTile) == null) {
+            return true;
         }
 
-        isMoving = false;
+        return false;
+    }
+
+    private void GetTargetPos() {
+        // A new target pos should not be calculated if the player is moving.
+        if (isMoving) return;
+
+        Vector2 movement = Vector2.zero;
+
+        switch (movementDirection)
+        {
+            case MovementDirection.UP:
+                movement = Vector2.up;
+                break;
+            case MovementDirection.DOWN:
+                movement = Vector2.down;
+                break;
+            case MovementDirection.LEFT:
+                movement = Vector2.left;
+                break;
+            case MovementDirection.RIGHT:
+                movement = Vector2.right;
+                break;
+        }
+        movePoint.position = rb.position + movement;
+        AnimateFaceDirection(movement);
+    }
+    
+    private void HandleInput() {
+        // Disable input when moving
+        if (isMoving) return;
+        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        
+        // If no input detected, exit
+        if (x == 0 && y == 0) return;
+
+        if (y != 0) {
+            if (y > 0) movementDirection = MovementDirection.UP;
+            else movementDirection = MovementDirection.DOWN;
+        }
+        else {
+            if (x > 0) movementDirection = MovementDirection.RIGHT;
+            else movementDirection = MovementDirection.LEFT;
+        }
+    }
+
+    private void Move()
+    {
+        Vector2 targetPos = movePoint.position;
+
+        // Check if the target position is reached
+        if (Vector2.Distance(rb.position, targetPos) > Mathf.Epsilon)
+        {
+            // Move towards the target position
+            rb.position = Vector2.MoveTowards(rb.position, targetPos, moveSpeed * Time.deltaTime);
+            isMoving = true;
+        }
+        else
+        {
+            // Stop moving
+            isMoving = false;
+            movementDirection = MovementDirection.NONE;
+        }
+    }
+
+    private void AnimateFaceDirection(Vector2 movement) {
+        animator.SetFloat(MoveX, movement.x);
+        animator.SetFloat(MoveY, movement.y);
+    }
+
+    private IEnumerator AnimateMovement()
+    {
+        Vector2 originalPos = rb.position;
+        yield return new WaitForSeconds(0.1f);
+
+        // Check if the player has moved during the delay
+        animator.SetBool(IsMoving, rb.position != originalPos);
     }
 }
